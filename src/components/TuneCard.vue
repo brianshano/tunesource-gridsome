d29280cd884d1c285186f827448400b27c917fb1
Brian Shanley <5247913+brianshano@users.noreply.github.com>
2020-05-01 15:18:51 +0100

    Abcjs css problem (#30)
    * stuff
    
    * fix abcjs css not showing

--- src/components/TuneCard.vue
+++ src/components/TuneCard.vue
@@ -244,7 +244,6 @@ import TuneLinker from '~/components/TuneLinker.vue';
 // const abcjs = require('abcjs');
 // window.abcjs = require(abcjs);
 // const synthControl = new abcjs.synth.SynthController();
-require('../abcjs-audio.css');
 // import 'abcjs/abcjs-audio.css';
 
 // import abcjs from 'abcjs';
