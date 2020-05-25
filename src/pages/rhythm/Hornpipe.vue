79f4935ba3a176df253ec13d601dbf3fd0d3e221
Brian Shanley <5247913+brianshano@users.noreply.github.com>
2020-05-25 23:34:44 +0100

    Tunes rhythms some (#35)
    * footer
    
    * share api
    
    * jigs n reels, hp and wtz

---
+++ src/pages/rhythm/Hornpipe.vue
@@ -0,0 +1,59 @@
+<template>
+  <Layout>
+    <section>
+      <div class="container home">
+        <div class="results-title text-white text-center block pt-1 pb-1 mt-4 mb-2">
+          <h1>Hornpipes</h1>
+        </div>
+        <div v-if="$page.allGoogleSheet.edges.length" class="tune-list">
+          <div
+            v-for="page in $page.allGoogleSheet.edges"
+            :key="page.tuneId"
+            class="w-full max-w-xs min-w-10 rounded overflow-hidden shadow-lg bg-gray-400 m-4"
+          >
+            <Card :tune="page.node" />
+          </div>
+        </div>
+        <div class="pagination">
+          <Pager :info="$page.allGoogleSheet.pageInfo" />
+        </div>
+      </div>
+    </section>
+  </Layout>
+</template>
+
+<script>
+import { Pager } from 'gridsome';
+import Card from '~/components/Card';
+
+export default {
+  components: {
+    Pager,
+    Card,
+  },
+};
+</script>
+
+<page-query>
+  query ($page: Int){
+    allGoogleSheet(perPage: 20, page: $page, filter: { rhythm: { eq: "hornpipe" }}) @paginate {
+      pageInfo {
+        totalPages
+        currentPage
+      }
+      edges {
+        node {
+          tuneId
+          id
+          title
+          shlug
+          abc
+          rhythm
+          key
+          abcheader
+          path
+        }
+      }
+    }
+  }
+</page-query>
