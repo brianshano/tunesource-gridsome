a27187cfc2834fa99f5558b32fa2c76b53d5f4ed
brianshano <bshanley@gmail.com>
2020-11-05 22:23:50 +0000

    fix pagination bogey styling

--- static/tailwind.config.js
+++ static/tailwind.config.js
@@ -1,47 +1,47 @@
-// tailwind.config.js
-// const plugin = require('tailwindcss/plugin');
-module.exports = {
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  // purge: {
-  //   enabled: true,
-  //   content: ['./src/**/*.html'],
-  //   options: {
-  //     whitelist: ['abcjs'],
-  //   },
-  },
+// // tailwind.config.js
+// // const plugin = require('tailwindcss/plugin');
+// module.exports = {
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {},
+//   // purge: {
+//   //   enabled: true,
+//   //   content: ['./src/**/*.html'],
+//   //   options: {
+//   //     whitelist: ['abcjs'],
+//   //   },
+//   },
 
-  // whitelistPatterns: [/abcjs/],
-  // whitelistPatterns: [/abcjs/],
-  // whitelistPatternsChildren: [/abcjs/],
+//   // whitelistPatterns: [/abcjs/],
+//   // whitelistPatterns: [/abcjs/],
+//   // whitelistPatternsChildren: [/abcjs/],
 
-  plugins: [
-    plugin(function ({ addComponents }) {
-      const buttons = {
-        '.btn': {
-          padding: '.5rem 1rem',
-          borderRadius: '.25rem',
-          fontWeight: '600',
-        },
-        '.btn-blue': {
-          backgroundColor: '#3490dc',
-          color: '#fff',
-          '&:hover': {
-            backgroundColor: '#2779bd',
-          },
-        },
-        '.btn-red': {
-          backgroundColor: '#e3342f',
-          color: '#fff',
-          '&:hover': {
-            backgroundColor: '#cc1f1a',
-          },
-        },
-      };
+//   plugins: [
+//     plugin(function ({ addComponents }) {
+//       const buttons = {
+//         '.btn': {
+//           padding: '.5rem 1rem',
+//           borderRadius: '.25rem',
+//           fontWeight: '600',
+//         },
+//         '.btn-blue': {
+//           backgroundColor: '#3490dc',
+//           color: '#fff',
+//           '&:hover': {
+//             backgroundColor: '#2779bd',
+//           },
+//         },
+//         '.btn-red': {
+//           backgroundColor: '#e3342f',
+//           color: '#fff',
+//           '&:hover': {
+//             backgroundColor: '#cc1f1a',
+//           },
+//         },
+//       };
 
-      addComponents(buttons);
-    }),
-  ],
-};
+//       addComponents(buttons);
+//     }),
+//   ],
+// };
